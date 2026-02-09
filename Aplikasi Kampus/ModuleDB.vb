7dd4ecf4eca6ee483c07266da41546c5e598c18c
unknown <aryandoprasetya97@gmail.com>
2026-02-10 01:11:18 +0700

    update

--- Aplikasi Kampus/ModuleDB.vb
+++ Aplikasi Kampus/ModuleDB.vb
@@ -1,14 +1,12 @@
-﻿Imports MySql.Data.MySqlClient
+﻿Imports MySql.Data.MySqlClient 'untuk koneksi MySQL
 
 Module ModuleDB
-    Public conn As MySqlConnection
-    Public cmd As MySqlCommand
-    Public dr As MySqlDataReader
-    Public da As MySqlDataAdapter
-    Public ds As DataSet
+    Public conn As MySqlConnection 'untuk koneksi ke database
+    Public cmd As MySqlCommand 'untuk eksekusi perintah SQL
+    Public dr As MySqlDataReader 'untuk membaca data hasil query
+    Public da As MySqlDataAdapter 'untuk mengisi dataset
+    Public ds As DataSet 'untuk menyimpan data dalam memori
 
-    ' PENTING: Ganti "PASSWORD_ANDA" dengan password yang Anda pakai di MySQL Workbench tadi.
-    ' Jika di Workbench tidak pakai password, hapus tulisan PASSWORD_ANDA (biarkan setelah sama dengan kosong).
     Public strKoneksi As String = "Server=localhost;Database=db_kampus;Uid=root;"
 
     Public Sub Koneksi()
